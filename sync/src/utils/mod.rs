a5ef9a29ba7421d759fd1ffd8ad599cf42ce1bd7
HAOYUatHZ <haoyu@protonmail.com>
2020-10-21 20:28:37 +0800

    remove partial_merkle_tree

--- sync/src/utils/mod.rs
+++ sync/src/utils/mod.rs
@@ -8,7 +8,7 @@ mod hash_queue;
 mod known_hash_filter;
 mod message_block_headers_provider;
 mod orphan_blocks_pool;
-mod partial_merkle_tree;
+// mod partial_merkle_tree;
 mod synchronization_state;
 
 pub use self::average_speed_meter::AverageSpeedMeter;
@@ -21,7 +21,7 @@ pub use self::hash_queue::{HashPosition, HashQueue, HashQueueChain};
 pub use self::known_hash_filter::{KnownHashFilter, KnownHashType};
 pub use self::message_block_headers_provider::MessageBlockHeadersProvider;
 pub use self::orphan_blocks_pool::OrphanBlocksPool;
-pub use self::partial_merkle_tree::{build_partial_merkle_tree, PartialMerkleTree};
+// pub use self::partial_merkle_tree::{build_partial_merkle_tree, PartialMerkleTree};
 pub use self::synchronization_state::SynchronizationState;
 
 /// Block height type
