91b16bf6698fa452ea5abb83ca905894b2631030
HAOYUatHZ <haoyu@protonmail.com>
2020-11-05 21:26:40 +0800

    clean up

--- spow/src/spow.rs
+++ spow/src/spow.rs
@@ -6,7 +6,6 @@ use std::vec::Vec;
 pub fn mine(
     step: u64,
     pubkey: &ecvrf::VrfPk,
-    modulus: &Integer,
     ini_state: &Integer,
     target: &Integer,
 ) -> (Integer, Vec<Integer>, u64) {
@@ -15,20 +14,19 @@ pub fn mine(
 
     loop {
         iters += step;
-        let (new_state, diff_valid) = solve(modulus, &cur_state, step, pubkey, target);
+        let (new_state, diff_valid) = solve(&cur_state, step, pubkey, target);
         cur_state = new_state;
         if diff_valid {
             break;
         }
     }
 
-    let pi = prove(modulus, ini_state, &cur_state, iters);
+    let pi = prove(ini_state, &cur_state, iters);
 
     (cur_state.clone(), pi, iters)
 }
 
 pub fn verify(
-    modulus: &Integer,
     g: &Integer,
     y: &Integer,
     iterations: u64,
@@ -47,24 +45,27 @@ pub fn verify(
     for mu_i in pi_list {
         let r_i = util::hash_fs(&[&x_i, &y_i, &mu_i]);
 
-        let xi_ri = x_i.clone().pow_mod(&r_i, modulus).unwrap();
-        x_i = (xi_ri * mu_i.clone()).div_rem_floor(modulus.clone()).1;
+        let xi_ri = x_i.clone().pow_mod(&r_i, &util::RSA2048_MODULUS).unwrap();
+        x_i = (xi_ri * mu_i.clone())
+            .div_rem_floor(util::RSA2048_MODULUS.clone())
+            .1;
 
-        let mui_ri = mu_i.clone().pow_mod(&r_i, modulus).unwrap();
-        y_i = (mui_ri * y_i.clone()).div_rem_floor(modulus.clone()).1;
+        let mui_ri = mu_i.clone().pow_mod(&r_i, &util::RSA2048_MODULUS).unwrap();
+        y_i = (mui_ri * y_i.clone())
+            .div_rem_floor(util::RSA2048_MODULUS.clone())
+            .1;
 
         t = t / 2;
         if (t % 2 != 0) && (t != 1) {
             t += 1;
-            y_i = y_i.clone().pow_mod(&two, modulus).unwrap();
+            y_i = y_i.clone().pow_mod(&two, &util::RSA2048_MODULUS).unwrap();
         }
     }
 
-    y_i == x_i.pow_mod(&two, modulus).unwrap()
+    y_i == x_i.pow_mod(&two, &util::RSA2048_MODULUS).unwrap()
 }
 
 pub fn solve(
-    modulus: &Integer,
     state: &Integer,
     step: u64,
     pubkey: &ecvrf::VrfPk,
@@ -73,15 +74,14 @@ pub fn solve(
     let mut y = state.clone();
     for _ in 0..step {
         y = y.clone() * y.clone();
-        // TODO:
-        y = y.div_rem_floor(modulus.clone()).1;
+        y = y.div_rem_floor(util::RSA2048_MODULUS.clone()).1;
     }
 
     let hstate = util::h_state(pubkey, &y);
     (y, util::validate_difficulty(&hstate, target))
 }
 
-pub fn prove(modulus: &Integer, g: &Integer, y: &Integer, iterations: u64) -> Vec<Integer> {
+pub fn prove(g: &Integer, y: &Integer, iterations: u64) -> Vec<Integer> {
     let (mut x_i, mut y_i) = (g.clone(), y.clone());
     let mut pi_list = Vec::<Integer>::new();
 
@@ -89,20 +89,27 @@ pub fn prove(modulus: &Integer, g: &Integer, y: &Integer, iterations: u64) -> Ve
     let two: Integer = 2u64.into();
     while t >= 2 {
         let two_exp = Integer::from(1) << ((t / 2) as u32); // 2^(t/2)
-        let mu_i = x_i.clone().pow_mod(&two_exp, modulus).unwrap();
+        let mu_i = x_i
+            .clone()
+            .pow_mod(&two_exp, &util::RSA2048_MODULUS)
+            .unwrap();
 
         let r_i = util::hash_fs(&[&x_i, &y_i, &mu_i]);
 
-        let xi_ri = x_i.clone().pow_mod(&r_i, modulus).unwrap();
-        x_i = (xi_ri * mu_i.clone()).div_rem_floor(modulus.clone()).1;
+        let xi_ri = x_i.clone().pow_mod(&r_i, &util::RSA2048_MODULUS).unwrap();
+        x_i = (xi_ri * mu_i.clone())
+            .div_rem_floor(util::RSA2048_MODULUS.clone())
+            .1;
 
-        let mui_ri = mu_i.clone().pow_mod(&r_i, modulus).unwrap();
-        y_i = (mui_ri * y_i.clone()).div_rem_floor(modulus.clone()).1;
+        let mui_ri = mu_i.clone().pow_mod(&r_i, &util::RSA2048_MODULUS).unwrap();
+        y_i = (mui_ri * y_i.clone())
+            .div_rem_floor(util::RSA2048_MODULUS.clone())
+            .1;
 
         t = t / 2;
         if (t % 2 != 0) && (t != 1) {
             t += 1;
-            y_i = y_i.clone().pow_mod(&two, modulus).unwrap();
+            y_i = y_i.clone().pow_mod(&two, &util::RSA2048_MODULUS).unwrap();
         }
 
         pi_list.push(mu_i);
