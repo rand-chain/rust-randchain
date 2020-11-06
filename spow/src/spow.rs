5ea77c552ada4c8b7f43ae87dab14763dbde3397
HAOYUatHZ <haoyu@protonmail.com>
2020-11-06 12:38:41 +0800

    refactor

--- spow/src/spow.rs
+++ spow/src/spow.rs
@@ -1,4 +1,4 @@
-use ecvrf;
+use ecvrf::VrfPk;
 use rug::Integer;
 use std::vec::Vec;
 
@@ -6,8 +6,13 @@ use super::config::{MODULUS, STEP};
 use super::util;
 
 ///
-/// type definitions
+/// Sequetial Proof-of-Work
 ///
+
+#[derive(Debug)]
+pub struct SPoW<'a> {
+    pubkey: &'a VrfPk,
+}
 
 pub type Proof = Vec<Integer>;
 
@@ -19,77 +24,82 @@ pub struct SPoWResult {
     proof: Proof,
 }
 
-///
-/// Sequential Proof-of-Work logic functions
-///
+impl SPoW<'_> {
+    pub fn new(pubkey: &'static VrfPk) -> Self {
+        SPoW { pubkey: pubkey }
+    }
 
-pub fn mine(pubkey: &ecvrf::VrfPk, ini_state: &Integer, target: &Integer) -> SPoWResult {
-    let mut cur_state = ini_state.clone();
-    let mut iters: u64 = 0;
+    pub fn mine(&mut self, ini_state: &Integer, target: &Integer) -> SPoWResult {
+        let mut cur_state = ini_state.clone();
+        let mut iters: u64 = 0;
 
-    loop {
-        iters += STEP;
-        let (new_state, diff_valid) = solve(&cur_state, pubkey, target);
-        cur_state = new_state;
-        if diff_valid {
-            break;
+        loop {
+            iters += STEP;
+            let (new_state, diff_valid) = self.solve(&cur_state, target);
+            cur_state = new_state;
+            if diff_valid {
+                break;
+            }
+        }
+
+        SPoWResult {
+            iterations: iters,
+            randomness: cur_state.clone(),
+            proof: prove(ini_state, &cur_state, iters),
         }
     }
 
-    SPoWResult {
-        iterations: iters,
-        randomness: cur_state.clone(),
-        proof: prove(ini_state, &cur_state, iters),
-    }
-}
-
-pub fn verify(
-    g: &Integer,
-    y: &Integer,
-    iterations: u64,
-    proof: &Proof,
-    pubkey: &ecvrf::VrfPk,
-    target: &Integer,
-) -> bool {
-    let hstate = util::h_state(pubkey, y);
-    if !util::validate_difficulty(&hstate, target) {
-        return false;
-    }
-
-    let (mut x_i, mut y_i) = (g.clone(), y.clone());
-    let mut t = iterations;
-    let two: Integer = 2u64.into();
-    for mu_i in proof {
-        let r_i = util::hash_fs(&[&x_i, &y_i, &mu_i]);
-
-        let xi_ri = x_i.clone().pow_mod(&r_i, &MODULUS).unwrap();
-        x_i = (xi_ri * mu_i.clone()).div_rem_floor(MODULUS.clone()).1;
-
-        let mui_ri = mu_i.clone().pow_mod(&r_i, &MODULUS).unwrap();
-        y_i = (mui_ri * y_i.clone()).div_rem_floor(MODULUS.clone()).1;
-
-        t = t / 2;
-        if (t % 2 != 0) && (t != 1) {
-            t += 1;
-            y_i = y_i.clone().pow_mod(&two, &MODULUS).unwrap();
+    pub fn solve(&mut self, state: &Integer, target: &Integer) -> (Integer, bool) {
+        let mut y = state.clone();
+        for _ in 0..STEP {
+            y = y.clone() * y.clone();
+            y = y.div_rem_floor(MODULUS.clone()).1;
         }
+
+        let hstate = util::h_state(self.pubkey, &y);
+        (y, util::validate_difficulty(&hstate, target))
     }
 
-    y_i == x_i.pow_mod(&two, &MODULUS).unwrap()
-}
+    pub fn verify(
+        &mut self,
+        g: &Integer,
+        y: &Integer,
+        iterations: u64,
+        proof: &Proof,
+        target: &Integer,
+    ) -> bool {
+        let hstate = util::h_state(self.pubkey, y);
+        if !util::validate_difficulty(&hstate, target) {
+            return false;
+        }
 
-pub fn solve(state: &Integer, pubkey: &ecvrf::VrfPk, target: &Integer) -> (Integer, bool) {
-    let mut y = state.clone();
-    for _ in 0..STEP {
-        y = y.clone() * y.clone();
-        y = y.div_rem_floor(MODULUS.clone()).1;
+        let (mut x_i, mut y_i) = (g.clone(), y.clone());
+        let mut t = iterations;
+        let two: Integer = 2u64.into();
+        for mu_i in proof {
+            let r_i = util::hash_fs(&[&x_i, &y_i, &mu_i]);
+
+            let xi_ri = x_i.clone().pow_mod(&r_i, &MODULUS).unwrap();
+            x_i = (xi_ri * mu_i.clone()).div_rem_floor(MODULUS.clone()).1;
+
+            let mui_ri = mu_i.clone().pow_mod(&r_i, &MODULUS).unwrap();
+            y_i = (mui_ri * y_i.clone()).div_rem_floor(MODULUS.clone()).1;
+
+            t = t / 2;
+            if (t % 2 != 0) && (t != 1) {
+                t += 1;
+                y_i = y_i.clone().pow_mod(&two, &MODULUS).unwrap();
+            }
+        }
+
+        y_i == x_i.pow_mod(&two, &MODULUS).unwrap()
     }
-
-    let hstate = util::h_state(pubkey, &y);
-    (y, util::validate_difficulty(&hstate, target))
 }
 
-pub fn prove(g: &Integer, y: &Integer, iterations: u64) -> Vec<Integer> {
+///
+/// helper function
+///
+fn prove(g: &Integer, y: &Integer, iterations: u64) -> Proof {
     let (mut x_i, mut y_i) = (g.clone(), y.clone());
     let mut proof = Proof::new();
 
@@ -118,5 +128,3 @@ pub fn prove(g: &Integer, y: &Integer, iterations: u64) -> Vec<Integer> {
 
     proof
 }
-
-// TODO: tests
