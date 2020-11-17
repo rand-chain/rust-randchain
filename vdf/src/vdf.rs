6b081a2066d35623fb7ce6893818e8f0db895578
HAOYUatHZ <haoyu@protonmail.com>
2020-11-18 04:55:53 +0800

    minor

--- vdf/src/vdf.rs
+++ vdf/src/vdf.rs
@@ -21,7 +21,7 @@ pub fn prove(g: &Integer, y: &Integer, iterations: u32) -> Proof {
     let mut proof = Proof::new();
 
     let mut t = iterations;
-    let two: Integer = 2.into();
+    let two = Integer::from(2);
     while t >= 2 {
         let two_exp = Integer::from(1) << (t / 2); // 2^(t/2)
         let mu_i = x_i.clone().pow_mod(&two_exp, &MODULUS).unwrap();
@@ -49,7 +49,7 @@ pub fn prove(g: &Integer, y: &Integer, iterations: u32) -> Proof {
 pub fn verify(g: &Integer, y: &Integer, iterations: u32, proof: &Proof) -> bool {
     let (mut x_i, mut y_i) = (g.clone(), y.clone());
     let mut t = iterations;
-    let two: Integer = 2.into();
+    let two = Integer::from(2);
     for mu_i in proof {
         let r_i = util::hash_fs(&[&x_i, &y_i, &mu_i]);
 
