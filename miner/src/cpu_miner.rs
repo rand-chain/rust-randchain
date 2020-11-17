7fb3f5608827e6713ce752f5067fb92b423a0bb6
HAOYUatHZ <haoyu@protonmail.com>
2020-11-17 15:53:40 +0800

    do

--- miner/src/cpu_miner.rs
+++ miner/src/cpu_miner.rs
@@ -74,25 +74,19 @@ pub fn find_solution(
         block.bits,
     );
 
-    while extranonce < max_extranonce {
-        // extranonce.to_little_endian(&mut extranonce_bytes);
+    for nonce in 0..(u32::max_value() as u64 + 1) {
+        // update §
+        // header_bytes.set_nonce(nonce as u32);
+        let hash = header_bytes.fill_and_hash(pubkey, nonce, Integer::from(0), vec![]);
+        if is_valid_proof_of_work_hash(block.bits, &hash) {
+            let solution = Solution {
+                nonce: nonce as u32,
+                randomness: randomness,
+                proof: proof,
+            };
 
-        for nonce in 0..(u32::max_value() as u64 + 1) {
-            // update §
-            // header_bytes.set_nonce(nonce as u32);
-            let hash = header_bytes.fill_and_hash(pubkey, nonce, Integer::from(0), vec![]);
-            if is_valid_proof_of_work_hash(block.bits, &hash) {
-                let solution = Solution {
-                    nonce: nonce as u32,
-                    randomness: randomness,
-                    proof: proof,
-                };
-
-                return Some(solution);
-            }
+            return Some(solution);
         }
-
-        extranonce = extranonce + 1.into();
     }
 
     None
