d4bae6346e63017a4abcc17c211573b8ccf0987f
HAOYUatHZ <haoyu@protonmail.com>
2020-11-17 22:56:45 +0800

    fix

--- miner/src/cpu_miner.rs
+++ miner/src/cpu_miner.rs
@@ -38,14 +38,13 @@ pub struct Solution {
 pub fn find_solution(block: &BlockTemplate, pubkey: VrfPk) -> Option<Solution> {
     let g = h_g(block, pubkey);
     let mut cur_y = g.clone();
-    for nonce in 0..u32::max_value() {
+    for nonce in 1..u32::max_value() {
         let new_y = vdf::eval(&cur_y, STEP);
         if is_valid_proof_of_work_hash(block.bits, &dhash256(new_y.to_string_radix(16).as_ref())) {
             let solution = Solution {
                 nonce: nonce as u32,
                 randomness: new_y.clone(),
-                // TODO: handle overflow
-                proof: vdf::prove(&g, &new_y, u64::from(nonce + 1) * STEP),
+                proof: vdf::prove(&g, &new_y, u64::from(nonce) * STEP),
             };
 
             return Some(solution);
