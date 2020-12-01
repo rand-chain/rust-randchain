f4222761ee1d003146c7d056f4035647c0843371
Runchao Han <runchao.han@monash.edu>
2020-12-01 16:14:10 +0800

    genesis block

--- network/src/network.rs
+++ network/src/network.rs
@@ -1,6 +1,9 @@
 //! randchain network
 
-use chain::IndexedBlock;
+extern crate rug;
+extern crate ecvrf;
+
+use chain::{IndexedBlock, BlockHeader, Block};
 use compact::Compact;
 use primitives::bigint::U256;
 use primitives::hash::H256;
@@ -85,9 +88,51 @@ impl Network {
 
     pub fn genesis_block(&self) -> IndexedBlock {
         match *self {
-			Network::Mainnet | Network::Other(_) => "0100000000000000000000000000000000000000000000000000000000000000000000003ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a29ab5f49ffff001d1dac2b7c0101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4d04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73ffffffff0100f2052a01000000434104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac00000000".into(),
-			Network::Testnet => "0100000000000000000000000000000000000000000000000000000000000000000000003ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4adae5494dffff001d1aa4ae180101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4d04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73ffffffff0100f2052a01000000434104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac00000000".into(),
-			Network::Regtest | Network::Unitest => "0100000000000000000000000000000000000000000000000000000000000000000000003ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4adae5494dffff7f20020000000101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4d04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73ffffffff0100f2052a01000000434104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac00000000".into(),
+			Network::Mainnet | Network::Other(_) => {
+                let blk = Block {
+                    block_header: BlockHeader {
+                        version: 1,
+                        previous_header_hash: [0; 32].into(), // should be zero, otherwise there will be error UnknownParent
+                        time: 4,
+                        bits: 5.into(),
+                        pubkey: ecvrf::VrfPk::from_bytes(&[6; 32]).unwrap(),
+                        iterations: 100000,
+                        randomness: rug::Integer::from(8),
+                    },
+                    proof: vec!()
+                };
+                IndexedBlock::from_raw(blk)
+            },
+			Network::Testnet => {
+                let blk = Block {
+                    block_header: BlockHeader {
+                        version: 1,
+                        previous_header_hash: [0; 32].into(), // should be zero, otherwise there will be error UnknownParent
+                        time: 4,
+                        bits: 5.into(),
+                        pubkey: ecvrf::VrfPk::from_bytes(&[6; 32]).unwrap(),
+                        iterations: 100000,
+                        randomness: rug::Integer::from(8),
+                    },
+                    proof: vec!()
+                };
+                IndexedBlock::from_raw(blk)
+            },
+			Network::Regtest | Network::Unitest => {
+                let blk = Block {
+                    block_header: BlockHeader {
+                        version: 1,
+                        previous_header_hash: [0; 32].into(), // should be zero, otherwise there will be error UnknownParent
+                        time: 4,
+                        bits: 5.into(),
+                        pubkey: ecvrf::VrfPk::from_bytes(&[6; 32]).unwrap(),
+                        iterations: 100000,
+                        randomness: rug::Integer::from(8),
+                    },
+                    proof: vec!()
+                };
+                IndexedBlock::from_raw(blk)
+            },
 		}
     }
 
