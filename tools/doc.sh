98e28e7c1499c87cbc0db13890ffc4045e01107b
Runchao Han <runchao.han@monash.edu>
2020-12-03 22:09:35 +0800

    revert doc.sh

--- tools/doc.sh
+++ tools/doc.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-cargo doc --no-deps --workspace
+cargo doc --no-deps\
+	-p bitcrypto\
+	-p chain\
+	-p db\
+	-p import\
+	-p keys\
+	-p message\
+	-p miner\
+	-p network\
+	-p randchaind\
+	-p p2p\
+	-p primitives\
+	-p rpc\
+	-p script\
+	-p serialization\
+	-p sync\
+	-p test-data\
+	-p verification
