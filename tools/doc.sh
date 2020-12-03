d7b10a2dc1b38857ac61a3d52884235eb5d496f2
Runchao Han <runchao.han@monash.edu>
2020-12-03 11:52:44 +0800

    generate doc script

--- tools/doc.sh
+++ tools/doc.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
 
-cargo doc --no-deps\
-	-p bitcrypto\
-	-p chain\
-	-p db\
-	-p import\
-	-p keys\
-	-p message\
-	-p miner\
-	-p network\
-	-p randchaind\
-	-p p2p\
-	-p primitives\
-	-p rpc\
-	-p script\
-	-p serialization\
-	-p sync\
-	-p test-data\
-	-p verification
+cargo doc --no-deps --workspace
