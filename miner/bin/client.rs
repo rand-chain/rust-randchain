4d2bad4d07798ecfaf7a76a35d1dcfcdbf114b78
HAOYUatHZ <haoyu@protonmail.com>
2020-12-07 19:02:46 +0800

    update clap

--- miner/bin/client.rs
+++ miner/bin/client.rs
@@ -1,3 +1,46 @@
+extern crate clap;
+
+use clap::Clap;
+
+/// RandChain miner client
+#[derive(Clap)]
+struct Opts {
+    #[clap(subcommand)]
+    command: SubCommand,
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    /// Generate a key pair
+    GenKey(GenkeyOpts),
+    /// Connect to randchaind rpc port and mine with a key
+    Mine(MineOpts),
+}
+
+/// A subcommand for ....
+#[derive(Clap)]
+struct GenkeyOpts {}
+
+/// A subcommand for ....
+#[derive(Clap)]
+struct MineOpts {}
+
 fn main() {
-	unimplemented!();
+    let opts: Opts = Opts::parse();
+    match opts.command {
+        SubCommand::GenKey(o) => {
+            gen_key(o);
+        }
+        SubCommand::Mine(o) => {
+            mine(o);
+        }
+    }
+}
+
+fn gen_key(opts: GenkeyOpts) {
+    unimplemented!();
+}
+
+fn mine(opts: MineOpts) {
+    unimplemented!();
 }
