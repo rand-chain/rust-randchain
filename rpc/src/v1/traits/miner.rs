f74a184e5e87056e8eedc86e4b3bbf8f17f3e895
HAOYUatHZ <haoyu@protonmail.com>
2020-12-09 10:48:52 +0800

    update rpc docs

--- rpc/src/v1/traits/miner.rs
+++ rpc/src/v1/traits/miner.rs
@@ -6,7 +6,7 @@ build_rpc_trait! {
     /// Parity-randchain miner data interface.
     pub trait Miner {
         /// Get block template for mining.
-        /// @curl-example: curl --data-binary '{"jsonrpc": "2.0", "method": "getblocktemplate", "params": [{"capabilities": ["coinbasetxn", "workid", "coinbase/append"]}], "id":1 }' -H 'content-type: application/json' http://127.0.0.1:8332/
+        /// @curl-example: curl --data-binary '{"jsonrpc": "2.0", "method": "getblocktemplate", "params": [{}], "id":1 }' -H 'content-type: application/json' http://127.0.0.1:8332/
         #[rpc(name = "getblocktemplate")]
         fn get_block_template(&self, BlockTemplateRequest) -> Result<BlockTemplate, Error>;
     }
