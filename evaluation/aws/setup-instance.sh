b76ed46bfe449cdc3a4101352858a79caeb7072c
Runchao Han <runchao.han@monash.edu>
2020-12-09 17:13:30 +0800

    update setup instance files

--- evaluation/aws/setup-instance.sh
+++ evaluation/aws/setup-instance.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-yum install -y python3 dstat
-python3 -m pip install --upgrade pip
-python3 -m pip install pyzmq pytest sympy
-
-# sympy is only required to run all tests
+yum install cmake gcc m4 
+yum install gmp gmp-devel mpfr mpfr-devel libmpc libmpc-devel
