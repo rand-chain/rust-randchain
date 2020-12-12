10edbfd3835e32c2821360c5afbad8001a4505ce
HAOYUatHZ <haoyu@protonmail.com>
2020-12-12 23:42:38 +0800

    use duration for miner's timeout

--- miner/src/cpu_miner.rs
+++ miner/src/cpu_miner.rs
@@ -1,4 +1,4 @@
-use std::time::Instant;
+use std::time::{Duration, Instant};
 
 use block_assembler::BlockTemplate;
 use chain::BlockHeader;
@@ -47,14 +47,14 @@ pub struct Solution {
 }
 
 /// Simple randchain cpu miner.
-pub fn find_solution(block: &BlockTemplate, pubkey: &VrfPk, timeout: u64) -> Option<Solution> {
+pub fn find_solution(block: &BlockTemplate, pubkey: &VrfPk, timeout: Duration) -> Option<Solution> {
     let start_time = Instant::now();
 
     let g = h_g(block, pubkey);
     let mut cur_y = g.clone();
     let mut iterations = 0u64;
     loop {
-        if timeout > 0 && timeout > start_time.elapsed().as_secs() {
+        if timeout != Duration::new(0, 0) && start_time.elapsed() > timeout {
             return None;
         }
 
@@ -94,6 +94,7 @@ mod tests {
     use block_assembler::BlockTemplate;
     use ecvrf::VrfPk;
     use primitives::bigint::{Uint, U256};
+    use std::time::Duration;
 
     #[test]
     fn test_cpu_miner_low_difficulty() {
@@ -107,7 +108,7 @@ mod tests {
 
         // generate or load key
         let pubkey: VrfPk = VrfPk::from_bytes(&[0; 32]).unwrap();
-        let solution = find_solution(&block_template, &pubkey, 0);
+        let solution = find_solution(&block_template, &pubkey, Duration::from_secs(0));
         assert!(solution.is_some());
     }
 }
