55e4d6eaed03a4b1eca2a6986cbf52a523353db7
HAOYUatHZ <haoyu@protonmail.com>
2020-12-14 09:01:12 +0800

    init

--- rpc/src/v1/traits/miner.rs
+++ rpc/src/v1/traits/miner.rs
@@ -9,5 +9,10 @@ build_rpc_trait! {
         /// @curl-example: curl --data-binary '{"jsonrpc": "2.0", "method": "getblocktemplate", "params": [{"capabilities": ["coinbasetxn", "workid", "coinbase/append"]}], "id":1 }' -H 'content-type: application/json' http://127.0.0.1:8332/
         #[rpc(name = "getblocktemplate")]
         fn get_block_template(&self, BlockTemplateRequest) -> Result<BlockTemplate, Error>;
+
+		/// Submit mined block.
+        /// TODO: @curl-example:
+        #[rpc(name = "submitblock")]
+        fn submit_block(&self, SubmitBlockRequest) -> Result<SubmitBlockResponse, Error>;
     }
 }
