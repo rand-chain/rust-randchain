56618a1d13dc44d0c05efb15247a9aee570028ce
HAOYUatHZ <haoyu@protonmail.com>
2020-12-14 09:33:21 +0800

    up up up

--- rpc/src/v1/types/submit_block.rs
+++ rpc/src/v1/types/submit_block.rs
@@ -1,5 +1,15 @@
 #[derive(Debug, Default, Serialize, Deserialize, PartialEq, Eq, Clone)]
-pub struct SubmitBlockRequest {}
+pub struct SubmitBlockRequest {
+	// TODO: use indexed_block?
+    // pub version: u32,
+    // pub previous_header_hash: H256,
+    // pub time: u32,
+    // pub bits: Compact,
+    // pub pubkey: VrfPk,
+    // pub iterations: u32,
+    // pub randomness: Integer,
+    // pub proof: vdf::Proof,
+}
 
 #[derive(Debug, Default, Serialize, Deserialize, PartialEq, Eq, Clone)]
 pub struct SubmitBlockResponse {}
