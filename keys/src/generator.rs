58bf4d35aec0198e5118be3277897c80cea4d836
Runchao Han <runchao.han@monash.edu>
2021-08-04 20:55:43 +1000

    init

---
+++ keys/src/generator.rs
@@ -0,0 +1,25 @@
+use network::Network;
+use {Error, KeyPair, SECP256K1};
+
+pub trait Generator {
+	fn generate(&self) -> Result<KeyPair, Error>;
+}
+
+pub struct Random {
+	network: Network,
+}
+
+impl Random {
+	pub fn new(network: Network) -> Self {
+		Random { network: network }
+	}
+}
+
+impl Generator for Random {
+	fn generate(&self) -> Result<KeyPair, Error> {
+		let context = &SECP256K1;
+		let mut rng = rand::thread_rng();
+		let (secret, public) = context.generate_keypair(&mut rng)?;
+		Ok(KeyPair::from_keypair(secret, public, self.network))
+	}
+}
