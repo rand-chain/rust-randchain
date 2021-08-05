2b875f5dcfb27b3dcdc0753c9da5a76ed87d7932
Runchao Han <runchao.han@monash.edu>
2021-08-05 22:33:50 +1000

    init sr25519

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -9,6 +9,7 @@ extern crate siphasher;
 
 pub mod ecvrf;
 mod hash;
+pub mod sr25519;
 
 pub use hash::{checksum, dhash160, dhash256, siphash24, DHash160, DHash256};
 pub use rcrypto::digest::Digest;
