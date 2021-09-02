f067af22a3c0e85bc435ec1430ece69cd60add90
Runchao Han <runchao.han@monash.edu>
2021-09-02 20:59:27 +1000

    refactor sr25519

--- crypto/src/sr25519.rs
+++ crypto/src/sr25519.rs
@@ -7,11 +7,11 @@ use schnorrkel::{ExpansionMode, Keypair, MiniSecretKey, PublicKey, SecretKey, Si
 /// returned vector is the concatenation of
 /// 1. the secret key (64 bytes)
 /// 2. the public key (32 bytes)
-pub fn keypair_from_seed(seed: &[u8]) -> Vec<u8> {
+pub fn keypair_from_seed(seed: &[u8]) -> (SecretKey, PublicKey) {
     match MiniSecretKey::from_bytes(seed) {
         Ok(mini) => {
             let keypair = mini.expand_to_keypair(ExpansionMode::Ed25519);
-            return keypair.to_bytes().to_vec();
+            (keypair.secret.clone(), keypair.public.clone())
         }
         Err(_) => panic!("Provided seed is invalid."),
     }
@@ -51,12 +51,9 @@ fn create_public(public_key: &[u8]) -> PublicKey {
 /// * message: Arbitrary length UIntArray
 ///
 /// * returned vector is the signature consisting of 64 bytes.
-pub fn sign(public_key: &[u8], secret_key: &[u8], message: &[u8]) -> Vec<u8> {
+pub fn sign(pk: &PublicKey, sk: &SecretKey, message: &[u8]) -> Vec<u8> {
     let context = b"";
-    create_secret(secret_key)
-        .sign_simple(context, message, &create_public(public_key))
-        .to_bytes()
-        .to_vec()
+    sk.sign_simple(context, message, pk).to_bytes().to_vec()
 }
 
 /// Verify a message and its corresponding against a public key;
@@ -64,23 +61,21 @@ pub fn sign(public_key: &[u8], secret_key: &[u8], message: &[u8]) -> Vec<u8> {
 /// * signature: UIntArray with 64 element
 /// * message: Arbitrary length UIntArray
 /// * pubkey: UIntArray with 32 element
-pub fn verify(signature: &[u8], message: &[u8], public_key: &[u8]) -> bool {
+pub fn verify(signature: &[u8], message: &[u8], pk: &PublicKey) -> bool {
     let context = b"";
     let signature = match Signature::from_bytes(signature) {
         Ok(signature) => signature,
         Err(_) => return false,
     };
 
-    create_public(public_key)
-        .verify_simple(context, message, &signature)
-        .is_ok()
+    pk.verify_simple(context, message, &signature).is_ok()
 }
 
 #[cfg(test)]
 pub mod tests {
     use super::*;
     use hex_literal::hex;
-    use schnorrkel::{KEYPAIR_LENGTH, SECRET_KEY_LENGTH, SIGNATURE_LENGTH};
+    use schnorrkel::{KEYPAIR_LENGTH, PUBLIC_KEY_LENGTH, SECRET_KEY_LENGTH, SIGNATURE_LENGTH};
 
     fn generate_random_seed() -> Vec<u8> {
         (0..32).map(|_| rand::random::<u8>()).collect()
@@ -89,29 +84,27 @@ pub mod tests {
     #[test]
     fn can_create_keypair() {
         let seed = generate_random_seed();
-        let keypair = keypair_from_seed(seed.as_slice());
+        let (sk, pk) = keypair_from_seed(seed.as_slice());
 
-        assert!(keypair.len() == KEYPAIR_LENGTH);
+        assert!(sk.to_bytes().len() == SECRET_KEY_LENGTH);
+        assert!(pk.to_bytes().len() == PUBLIC_KEY_LENGTH);
     }
 
     #[test]
     fn creates_pair_from_known() {
         let seed = hex!("fac7959dbfe72f052e5a0c3c8d6530f202b02fd8f9f5ca3580ec8deb7797479e");
         let expected = hex!("46ebddef8cd9bb167dc30878d7113b7e168e6f0646beffd77d69d39bad76b47a");
-        let keypair = keypair_from_seed(&seed);
-        let public = &keypair[SECRET_KEY_LENGTH..KEYPAIR_LENGTH];
+        let (sk, pk) = keypair_from_seed(&seed);
 
-        assert_eq!(public, expected);
+        assert_eq!(pk.to_bytes(), expected);
     }
 
     #[test]
     fn can_sign_message() {
         let seed = generate_random_seed();
-        let keypair = keypair_from_seed(seed.as_slice());
-        let private = &keypair[0..SECRET_KEY_LENGTH];
-        let public = &keypair[SECRET_KEY_LENGTH..KEYPAIR_LENGTH];
+        let (sk, pk) = keypair_from_seed(seed.as_slice());
         let message = b"this is a message";
-        let signature = sign(public, private, message);
+        let signature = sign(&pk, &sk, message);
 
         assert!(signature.len() == SIGNATURE_LENGTH);
     }
@@ -119,12 +112,10 @@ pub mod tests {
     #[test]
     fn can_verify_message() {
         let seed = generate_random_seed();
-        let keypair = keypair_from_seed(seed.as_slice());
-        let private = &keypair[0..SECRET_KEY_LENGTH];
-        let public = &keypair[SECRET_KEY_LENGTH..KEYPAIR_LENGTH];
+        let (sk, pk) = keypair_from_seed(seed.as_slice());
         let message = b"this is a message";
-        let signature = sign(public, private, message);
+        let signature = sign(&pk, &sk, message);
 
-        assert!(verify(&signature[..], message, public));
+        assert!(verify(&signature[..], message, &pk));
     }
 }
