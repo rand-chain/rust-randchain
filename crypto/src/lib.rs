b410efe68e57f94ab74334482acee8b96a7258dc
Runchao Han <runchao.han@monash.edu>
2021-09-04 15:15:51 +1000

    merge vdf into crypto/

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -6,12 +6,17 @@ extern crate rand_core;
 extern crate schnorrkel;
 extern crate sha3;
 extern crate siphasher;
+#[macro_use]
+extern crate lazy_static;
+extern crate rug;
+extern crate sha2;
 
 #[macro_use]
 extern crate hex_literal;
 
 mod hash;
 mod sr25519;
+pub mod vdf;
 
 pub use hash::{checksum, dhash160, dhash256, siphash24, DHash160, DHash256};
 pub use rcrypto::digest::Digest;
