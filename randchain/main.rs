302308f65fb9361d1db375c1bc130a3b0cd0a486
Runchao Han <elvisage941102@gmail.com>
2021-09-05 15:57:41 +1000

    main: tidy arguments of subcommand `node` (#154)

--- randchain/main.rs
+++ randchain/main.rs
@@ -21,7 +21,6 @@ extern crate sync;
 extern crate verification;
 
 mod commands;
-mod config;
 mod utils;
 
 use app_dirs::AppInfo;
@@ -49,22 +48,21 @@ fn run() -> Result<(), String> {
     let yaml = load_yaml!("cli.yml");
     let app = clap::App::from_yaml(yaml).setting(clap::AppSettings::ArgRequiredElseHelp);
     let matches = app.get_matches();
-    let cfg = config::parse(&matches)?;
-
-    if !cfg.quiet {
-        if cfg!(windows) {
-            logs::init(LOG_INFO, logs::DateLogFormatter);
-        } else {
-            logs::init(LOG_INFO, logs::DateAndColorLogFormatter);
-        }
-    } else {
-        env_logger::init();
-    }
+    // let cfg = config::parse(&matches)?;
+    // if !cfg.quiet {
+    //     if cfg!(windows) {
+    //         logs::init(LOG_INFO, logs::DateLogFormatter);
+    //     } else {
+    //         logs::init(LOG_INFO, logs::DateAndColorLogFormatter);
+    //     }
+    // } else {
+    //     env_logger::init();
+    // }
 
     match matches.subcommand() {
         // ("import", Some(import_matches)) => commands::import(cfg, import_matches),
         // ("rollback", Some(rollback_matches)) => commands::rollback(cfg, rollback_matches),
-        ("node", Some(_node_matches)) => commands::node::start(cfg, _node_matches),
+        ("node", Some(node_matches)) => commands::node::start(node_matches),
         _ => Err("Please specify a subcommand".to_owned()),
     }
 }
